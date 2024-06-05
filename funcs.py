5d64a759a11cf7ef18e38c8f5fae70349eb2a42a
AndreyAndrey1980 <mandibul1@mail.ru>
2024-06-05 21:32:28 +0300

    import funcs

---
+++ funcs.py
@@ -0,0 +1,5 @@
+def funcion_plus(a,b):
+    return a+b
+
+def funcion_minus(a,b):
+    return a-b
---
+++ main.py
@@ -0,0 +1,5 @@
+from funcs import funcion_plus
+from funcs import funcion_minus
+
+print(funcion_plus(5, 6))
+print(funcion_minus(5, 6))
